5a1b66ce9a2a6a9e5d2b3c561260536532bf91d5
apiwat59 <32583026+apiwat59@users.noreply.github.com>
2019-10-04 23:17:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 // host = '172.16.153.122';	// hostname or IP address
-host = '127.0.0.1';	// hostname or IP address
+host = 'm16.cloudmqtt.com';	// hostname or IP address
 // host = '172.16.153.110';	// hostname or IP address
-port = 9001;
+port = 37495;
 topic = '#';		// topic to subscribe to
 useTLS = false;
-username = null;
-password = null;
+username = "cszwalpv";
+password = "HuFTaCshWOMC";
 // username = "jjolie";
 // password = "aa";
 
